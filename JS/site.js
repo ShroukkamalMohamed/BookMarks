35f5598281a79d5526adb42a8360d78bff523e79
shrouk kamal <shroukkamal15@gmail.com>
2023-12-22 23:47:39 +0200

    add validation modal

--- JS/site.js
+++ JS/site.js
@@ -1,36 +1,98 @@
 // ^====================>>>>>Elements<<<<<<<<<=======================
 var bookMarkName = document.getElementById("bookmarkname");
 var bookMarkUrl = document.getElementById("bookmarkurl");
-var bookmarkTable = document.getElementById("bookmarkTable");
-var bookmarkCard = document.getElementById("bookmarkCard");
+var bookmarkTableBody = document.getElementById("bookmarkTableBody");
+var deletebtn = document.getElementById("deletebtn");
+var visitbtn = document.getElementById("visitbtn");
+var btnsubmit = document.getElementById("btnsubmit");
+var bookmarkname = document.getElementById("bookmarkname");
+var bookmarkurl = document.getElementById("bookmarkurl");
+var modal = document.getElementById('modal');
+
 // *====================>>>>>Variables<<<<<<<<<======================
-var bookMarkList = [];
+var bookMarkList = [{ bookMarkName: `Youtube`, bookMarkUrl: `https://www.youtube.com/` }];
+DisplayBookMarks(bookMarkList);
+var vaildSiteName = false;
+var vaildUrl = false;
 // ~====================>>>>>Function<<<<<<<<<=======================
 function DisplayBookMarks(bookMarklst) {
-  var tbldata = "";
-  card = "";
-  for (var i = 0; i < bookMarkList.length; i++) {
-    tbldata = `<tr>
+
+    var tbldata = "";
+    for (var i = 0; i < bookMarklst.length; i++) {
+        tbldata += `<tr>
        <td>${i + 1}</td>
-       <td>${bookMarkList[i].bookMarkName}</td>
-       <td>${bookMarkList[i].bookMarkUrl}</td> </tr>`;
-    card = ` <div class="col-md-3 >
-      <h4>
-        ${bookMarkList[i].bookMarkName}
-      </h4>
-      <img src="images/Screenshot.png" height="250px" alt="" />
-    </div>
- `;
-  }
-  //   bookmarkTable.innerHTML += tbldata;
-  bookmarkCard.innerHTML += card;
+       <td>${bookMarklst[i].bookMarkName}</td>
+       <td>${bookMarklst[i].bookMarkUrl}</td> 
+       <td> <button class="btn" id="visitbtn" onclick="visitSiteClick(${i})" ><i class="fa-solid fa-eye"></i></button></td>
+       <td> <button class="btn" id="deletebtn" onclick="deleteClick(${i})"  ><i class="fa-regular fa-trash-can"></i></button></td>
+       </tr>
+       `;
+    }
+    bookmarkTableBody.innerHTML = tbldata;
+
+};
+bookmarkname.addEventListener("input", function formValidation() {
+
+    var siteNameValidation = /^([a-z]|[A-Z]){3,}$/;
+    var valid = siteNameValidation.test(bookmarkname.value);
+    if (valid) {
+        bookmarkname.classList.remove("is-invalid");
+        bookmarkname.classList.add("is-valid");
+        vaildSiteName = true;
+    } else {
+        bookmarkname.classList.remove("is-valid");
+        bookmarkname.classList.add("is-invalid");
+        vaildSiteName = false;
+    }
+})
+bookmarkurl.addEventListener("input", function formValidation() {
+
+    var urlValidation = /^(https?:\/\/)?(w{3}\.)?\w+\.com*$/;
+    var valid = urlValidation.test(bookmarkurl.value);
+    if (valid) {
+        bookmarkurl.classList.remove("is-invalid");
+        bookmarkurl.classList.add("is-valid");
+        vaildUrl = true;
+    } else {
+        bookmarkurl.classList.remove("is-valid");
+        bookmarkurl.classList.add("is-invalid");
+        vaildUrl = false;
+    }
+})
+function clear() {
+    bookmarkurl.value = "";
+    bookmarkname.value = "";
 }
 //? ==================>>>>>Events<<<<<<<<<<=========================
-function insert() {
-  var bookmark = {
-    bookMarkName: bookMarkName.value,
-    bookMarkUrl: bookMarkUrl.value,
-  };
-  bookMarkList.push(bookmark);
-  DisplayBookMarks(bookMarkList);
+btnsubmit.addEventListener("click", function insertNewSite() {
+    if (vaildSiteName && bookMarkUrl.value && bookMarkName.value && vaildUrl) {
+        var bookmark = {
+            bookMarkName: bookMarkName.value,
+            bookMarkUrl: bookMarkUrl.value,
+        };
+        bookMarkList.push(bookmark);
+        DisplayBookMarks(bookMarkList);
+        clear();
+    }
+    else {
+        modal.classList.remove("d-none");
+        modal.classList.add("d-flex");
+    }
+})
+btnclose.addEventListener("click", function () {
+    modal.classList.remove("d-flex");
+    modal.classList.add("d-none");
+
+})
+function visitSiteClick(index) {
+    var url = (bookMarkList[index].bookMarkUrl);
+    window.open(url, '_blank').focus();
+};
+function deleteClick(index) {
+    console.log(bookMarkList[index]);
+    bookMarkList.splice(index, 1);
+    DisplayBookMarks(bookMarkList);
+    console.log(bookMarkList);
 }
+
+
